bff261afbfb4b58bfa145c8180d3fe4f2f3172b5
Catherine Chu <catchuuu2103@gmail.com>
2026-07-31 23:35:12 -0400

    project text, skills, links galore

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,11 +1,28 @@
 import type { TypeProject } from '../types';
+import {gknow} from '../assets';
 
 export const projects: TypeProject[] = [
     {
         id: 'proj-1',
-        title: 'Example Project',
-        description: 'A short description',
-        tags: ['React', 'TypeScript'],
-        link: 'https://example.com',
+        title: 'G-Know',
+        description: 'Native IOS app designed for therapists to create, securely store, and share their patient genograms.',
+        tags: ['Swift'],
+        image: [gknow],
+        link: 'https://projects.nmi.cool/2024/nmc/gknow/',
+        repo: 'https://github.com/gknowapp/G-Know',
+    },
+    {
+        id: 'proj-2',
+        title: 'AI Extension',
+        description: 'Integrated an open-source llama model into a browser extension, delivering sustainability insights and eco-friendly recommendations on clothing pages.',
+        tags: ['Llama','JavaScript', 'React', 'Python'],
+        repo: 'coming soon...',
+    },
+    {
+        id: 'proj-3',
+        title: 'Uber Data Analytics',
+        description: 'A mobile app that helps users discover and purchase sustainable fashion options.',
+        tags: ['Python', 'GCP', 'Mage'],
+        repo: 'coming soon...',
     },
 ];
